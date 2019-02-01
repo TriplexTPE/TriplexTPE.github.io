b39f72893320bb1d1911b1f89ca33abdbff04c41
TriplexTPE <ruben.crawford@protonmail.com>
2019-02-01 20:46:28 +0100

    Updates

---
+++ 13-a15c44a4efa4b0e8ecfa.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{163:function(n,w,o){}}]);
+//# sourceMappingURL=13-a15c44a4efa4b0e8ecfa.js.map
--- src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography"
-import lincolnTheme from "typography-theme-lincoln"
-lincolnTheme.headerColor = '#486a9d'
-
-const typography = new Typography(lincolnTheme)
-
-export default typography
-export const rhythm = typography.rhythm
